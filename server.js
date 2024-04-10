899b6b75fd4a84e35dd0c838a5d6b710778da125
gpratap608 <113552039+gpratap608@users.noreply.github.com>
2024-04-10 23:01:10 +0530

    Weather API

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 import express from "express"
+import http from "http"
 
 const app = express()
 
@@ -6,6 +7,24 @@ const port = 8800
 
 app.get("/",(req,res)=>{
     res.send("Hello, Welcome to Ganesh's server")
+})
+
+app.get("/weatherData",async (req,res)=>{ 
+    try {
+        const response= await fetch('https://open-weather13.p.rapidapi.com/city/landon/EN',{
+            method:'GET',
+            headers:
+            {
+                'X-RapidAPI-Key': '6d9cfee14emshd0feb508d8579aep19aabejsna94af4aa2e5b',
+                'X-RapidAPI-Host': 'open-weather13.p.rapidapi.com'
+            }
+        
+        })
+        const result = await response.json()
+        res.send(result)
+    } catch (error) {
+        console.log(error)
+    }
 })
 
 app.listen(port,()=>{
